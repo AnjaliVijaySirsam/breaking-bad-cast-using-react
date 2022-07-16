af59c54a2803f79a3963c6c6ca91062680f9dd91
anjalisirsam <anjalisirsam97@gmail.com>
2022-07-16 16:17:55 +0530

    Updates

---
+++ precache-manifest.18a52f2de3920399c52363bb26ab92e4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87bbb160f07a4af0e6a910dc026e4209",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f04403200d16304d6c9",
+    "url": "/static/css/main.93ffd0a8.chunk.css"
+  },
+  {
+    "revision": "b0ed3f9c387db462ca11",
+    "url": "/static/js/2.4297815b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.4297815b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f04403200d16304d6c9",
+    "url": "/static/js/main.93166b85.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.93166b85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "332709f2db6ed43f42b2",
+    "url": "/static/js/runtime-main.3747d922.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/static/media/spinner.235b9b3e.gif"
+  }
+]);
