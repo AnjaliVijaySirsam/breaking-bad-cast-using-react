4b3808979db3e5df0d12d8346427959c94110da1
anjalisirsam <anjalisirsam97@gmail.com>
2022-07-16 16:31:51 +0530

    Updates

---
+++ precache-manifest.55c1c4add0bdf0b59b5053a1c5009365.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a6577b67a53b20a860f546714dcdd40",
+    "url": "/breaking-bad-cast-using-react/index.html"
+  },
+  {
+    "revision": "3be5446e0de5d63d56a9",
+    "url": "/breaking-bad-cast-using-react/static/css/main.46e9d88d.chunk.css"
+  },
+  {
+    "revision": "b0ed3f9c387db462ca11",
+    "url": "/breaking-bad-cast-using-react/static/js/2.4297815b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/breaking-bad-cast-using-react/static/js/2.4297815b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3be5446e0de5d63d56a9",
+    "url": "/breaking-bad-cast-using-react/static/js/main.4d4938c4.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/breaking-bad-cast-using-react/static/js/main.4d4938c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3e4899b21fcb9eac897",
+    "url": "/breaking-bad-cast-using-react/static/js/runtime-main.bbf2a48b.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/breaking-bad-cast-using-react/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/breaking-bad-cast-using-react/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/breaking-bad-cast-using-react/static/media/spinner.235b9b3e.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.18a52f2de3920399c52363bb26ab92e4.js"
+  "/breaking-bad-cast-using-react/precache-manifest.55c1c4add0bdf0b59b5053a1c5009365.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/breaking-bad-cast-using-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
